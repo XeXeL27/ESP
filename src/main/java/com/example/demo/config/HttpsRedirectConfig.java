96a97f5d14e8bc9465d356385b13f6097357147e
fabricio19102003 <melgarvegapedrofabricio@gmail.com>
2025-06-30 09:14:23 -0400

    HTTPS-Commit-30-06-2025

---
+++ src/main/java/com/example/demo/config/HttpsRedirectConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpsRedirectConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> containerCustomizer() {
+        return server -> server.addAdditionalTomcatConnectors(httpConnector());
+    }
+
+    private Connector httpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(7777); // Puerto HTTP viejo
+        connector.setSecure(false);
+        connector.setRedirectPort(8443); // Redirige a HTTPS
+        return connector;
+    }
+}
